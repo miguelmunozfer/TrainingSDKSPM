3784033ad5c2c4baed7fddfa5e79cb84f8aadb62
Miguel Muñoz <mmunozfer@MacBook-Pro-de-Miguel.local>
2024-04-08 15:59:55 +0200

    changes

--- Package.swift
+++ Package.swift
@@ -4,14 +4,15 @@ import PackageDescription
 let package = Package(
     name: "TrainingSDK",
     platforms: [
-       .iOS(.v13) // Asegúrate de ajustar esto a la versión mínima de iOS que soporte tu SDK.
+       .iOS(.v10) // Ajusta esto a la versión mínima de iOS que soporte tu SDK.
     ],
     products: [
         .library(
             name: "TrainingSDK",
-            targets: ["TrainingSDK"]),
+            targets: ["TrainingSDKWrapper"]),
     ],
     dependencies: [
+        // Dependencias de tu SDK
         .package(url: "https://github.com/Alamofire/Alamofire", .exact("5.4.0")),
         .package(url: "https://github.com/hmhv/YoutubePlayer-in-WKWebView", .upToNextMajor(from: "0.3.9")),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", .exact("5.0.0")),
@@ -20,7 +21,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrainingSDK",
-            path: "./TrainingSDK.xcframework" // Asegúrate de ajustar la ruta al .xcframework
+            path: "./TrainingSDK.xcframework"
+        ),
+        .target(
+            name: "TrainingSDKWrapper",
+            dependencies: [
+                "TrainingSDK",
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "YoutubePlayer_in_WKWebView", package: "YoutubePlayer-in-WKWebView"),
+                .product(name: "SwiftyJSON", package: "SwiftyJSON"),
+                .product(name: "SwiftDate", package: "SwiftDate")
+            ],
+            path: "Sources/TrainingSDKWrapper" // Esta es una ruta ficticia. Deberás crear una estructura que funcione para tu caso específico.
         )
     ]
 )
