8147aca436c2ffb096e58c20c06337301b0b4d3c
Miguel Muñoz <mmunozfer@MacBook-Pro-de-Miguel.local>
2024-04-08 15:52:26 +0200

    changes

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "TrainingSDK",
     platforms: [
-       .iOS(.v10) // Asegúrate de ajustar esto a la versión mínima de iOS que soporte tu SDK.
+       .iOS(.v13) // Asegúrate de ajustar esto a la versión mínima de iOS que soporte tu SDK.
     ],
     products: [
         .library(
@@ -12,7 +12,6 @@ let package = Package(
             targets: ["TrainingSDK"]),
     ],
     dependencies: [
-        // Dependencias de tu SDK
         .package(url: "https://github.com/Alamofire/Alamofire", .exact("5.4.0")),
         .package(url: "https://github.com/hmhv/YoutubePlayer-in-WKWebView", .upToNextMajor(from: "0.3.9")),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", .exact("5.0.0")),
